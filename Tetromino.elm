c0c2a52b540a358f78d8383d05bc0b2f78423fd4
Vibhanshu Bhardwaj <vib@v1020-wn-191-64.campus-dynamic.uwaterloo.ca>
2018-08-22 19:04:35 -0400

    twists and moves

--- Tetromino.elm
+++ Tetromino.elm
@@ -8,8 +8,11 @@ import List
 import Block exposing (Block)
 
 type alias Location  = (Int, Int)
-type alias Tetromino = { shape: List Location
-                        , block: Block
+type alias Tetromino = { shape: List Location,
+                         block: Block,
+                         pivot: { r : Float, c: Float},
+                         rows: Int,
+                         cols: Int
                         }
 
 toForm: Tetromino -> Form 
@@ -17,18 +20,96 @@ toForm: Tetromino -> Form
 toForm {shape, block} = 
     let form = Block.toForm block
         translate (row, col) = move ((toFloat col) * Block.size,
-                                      (toFloat row)* Block.size ) form 
+                                      (toFloat row) * Block.size ) form 
         forms = List.map translate shape
     in group forms     
 
 
 i: Tetromino
-i = { shape = [ (1, 0), (0,0), (-1,0), (-2,0)], block = Block Color.lightBlue}
+i = { shape = [ (1, 0), (0,0), (-1,0), (-2,0)], 
+        block = Block Color.lightBlue,
+        pivot = { r = -0.5, c = 0.5},
+        rows = 4,
+        cols = 1
+    }
 
 j: Tetromino
-j = {shape = [(1,0), (0, 0), (-1, -1), (-1, 0)], block = Block Color.blue}
+j = { shape = [(1,0), (0, 0), (-1, -1), (-1, 0)], 
+        block = Block Color.blue,
+        pivot = { r = 0.0, c = 0.0},
+        rows = 3,
+        cols = 2
+    }
+l: Tetromino
+l = { shape = [(1,0), (0, 0), (-1, 0), (-1, 1)], 
+        block = Block Color.orange,
+        pivot = { r = 0.0, c = 0.0},
+        rows = 3,
+        cols = 2
+    }
+z: Tetromino
+z = { shape = [(1,-1), (1, 0), (0, 0), (0, 1)], 
+        block = Block Color.red,
+        pivot = { r = 0.0, c = 0.0},
+        rows = 2,
+        cols = 3
+    }
+s: Tetromino
+s = { shape = [(0,0), (0, 1), (-1, -1), (-1, 0)], 
+        block = Block Color.green,
+        pivot = { r = 0.0, c = 0.0},
+        rows = 2,
+        cols = 3
+    }
+t: Tetromino
+t = { shape = [(0,-1), (0, 0), (0, 1), (-1, 0)], 
+        block = Block Color.purple,
+        pivot = { r = 0.0, c = 0.0},
+        rows = 2,
+        cols = 3
+    }
+o: Tetromino
+o = { shape = [(0,0), (0, 1), (-1, 0), (-1, 1)], 
+        block = Block Color.yellow,
+        pivot = { r = -0.5, c = 0.5},
+        rows = 2,
+        cols = 3
+    }
 
-tetro = j
+drawPivot: Tetromino -> Form
+
+drawPivot {pivot} = 
+    let piv = circle 5 |> filled Color.red
+        translate = move (pivot.c * Block.size, pivot.r * Block.size)
+    in translate piv
+
+rotateLocation : {r: Float, c: Float} -> Float -> Location -> Location
+rotateLocation pivot angle (row, col) = 
+    let original_r = (toFloat row) - pivot.r
+        original_c = (toFloat col) - pivot.c 
+        (s, c) = (sin(angle), cos(angle))
+        new_r = original_r * c - original_c * s
+        new_c = original_r * s + original_c * c 
+    in (round <| new_r + pivot.r, round <| new_c + pivot.c)
+
+rotateTetromino: Tetromino -> Tetromino
+
+rotateTetromino tetromino = 
+    let specificRotationLocation = rotateLocation tetromino.pivot (degrees 90)
+        newShape = List.map specificRotationLocation tetromino.shape
+    in { tetromino | shape = newShape , rows = tetromino.cols, cols = tetromino.rows}
+
+
+
+shift : Location -> Tetromino -> Tetromino
+shift (rows, cols) tetromino = 
+    let 
+        shiftLocation (r, c) = (r + rows, c + cols)
+        newShape = List.map shiftLocation tetromino.shape
+        newPivot = {r = tetromino.pivot.r + (toFloat rows), c = tetromino.pivot.c + (toFloat cols)}
+    in {tetromino | shape = newShape, pivot = newPivot}
+
+tetro =  shift (-9, 2) <|rotateTetromino t
 main: Html.Html msg
-main = Element.toHtml <|collage 400 400 [toForm tetro]
+main = Element.toHtml <|collage 1000 1000 [toForm tetro, drawPivot tetro]
 
